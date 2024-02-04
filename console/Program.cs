c850e2058a13a6791d1a7223794c36d0c456a932
Mathias Cabrera <mathiasdesign1@gmail.com>
2024-02-03 23:02:22 -0300

    second

---
+++ console/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Por favor ingrese un nombre: ");
+var name = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese su cargo: ");
+var position = Console.ReadLine();
+
+Console.WriteLine("Por favor ingrese su edad: "); 
+var age = Console.ReadLine();
+
+Console.WriteLine($"Mi nombre es {name}. Mi edad es {age}. Mi cargo es {position}");
